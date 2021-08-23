c31cac1cc82635ac1eaea56ff27c44a8cc0cb7b3
johnny <944671335@qq.com>
2021-08-23 18:25:23 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,25 @@ $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     console.log(options.url);
+    if (options.url.indexOf('/my') !== -1) {
+        //统一为有权限的headers 设置请求头
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //全局统一挂载complate回调函数
+    options.complate = function(res) {
+        // console.log('执行了complate回调');
+        // console.log(res);
+        //在complate回调函数中 可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败') {
+            //强制清空token
+            localStorage.
+            removeItem('token');
+            //强制跳转到登陆页面
+            location.href = '/login.html';
+        }
+
+    }
 })
